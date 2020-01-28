e1456584996e1ba87b25783657ca0c88b0a7913e
David <david.dionise@detroitlabsl.com>
2020-01-28 15:45:08 -0500

    added file writing and removed unused variables

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { MongoClient } = require("mongodb");
 const axios = require("axios");
-const _ = require("lodash");
+const { Console } = require("console");
+const fs = require("fs");
 
 const URL = process.env.MONGO_DB_URL;
 const DB_NAME = process.env.MONGO_DB_NAME;
@@ -9,6 +10,8 @@ const BACKPACK_API_KEY = process.env.BACKPACK_API_KEY || "";
 const LIMIT = process.env.LIMIT || 25;
 
 async function main() {
+  const writeStream = fs.createWriteStream(`${__dirname}/price-output.txt`);
+  const console = new Console(writeStream);
   const client = await MongoClient.connect(URL);
   const db = client.db(DB_NAME);
   const inspectionsCollection = db.collection("inspection");
@@ -17,7 +20,7 @@ async function main() {
     inspections.map((inspection) => (
       axios.get(`${BACKPACK_URL}/inspections/${inspection._id}/result?key=${BACKPACK_API_KEY}`)
         .then(({ data: { offerAmount } }) => {
-          const { lead : { vehicle, offer: { icoValue } } } = inspection;
+          const { lead : { offer: { icoValue } } } = inspection;
           return {
             "ICO Value": icoValue,
             "Final Offer": offerAmount,
